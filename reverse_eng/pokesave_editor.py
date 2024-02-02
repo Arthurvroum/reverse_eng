fcb509f8c5e15324184b58d4e9acb50224a4831a
Arthur <arthur.mazeau@epitech.eu>
2024-02-02 12:42:25 +0400

    ok

--- reverse_eng/pokesave_editor.py
+++ reverse_eng/pokesave_editor.py
@@ -4,10 +4,13 @@ if __name__ == "__main__":
     with open(sys.argv[1], "rb+") as f:
         sav = bytearray(f.read()) # This variable is an array containing each bytes of your save file. If you want to modify the byte at address 0x42DE to the value 0x83 for example you can simply use sav[0x42DE] = 0x83 (just like a normal array :-))
         # Write some code here
-        b = 8
-        while b != 14:
-            sav[0x2598 + b] = 0x41
-            b += 1
+        sav[0x2598] = 0x80
+        sav[0x2599] = 0x82
+        sav[0x259A] = 0x80
+        sav[0x259B] = 0x82
+        sav[0x259C] = 0x80
+        sav[0x259D] = 0x82
+        sav[0x259E] = 0x80
         checksum = 0xff
         for i in sav[0x2598:0x3523]:
             checksum -= i
